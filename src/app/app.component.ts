ee289e0df0196885cb2388f10a677ad206abe167
Mansi Bhatt <mansi.bhatt@voyagecontrol.com>
2022-01-21 10:15:23 +0530

    pass data parent to child and reusable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'Angular Satsang';
+  users=[{
+    name:"mansi",
+    age:20,
+    add:"Ahmd"
+  },
+  {
+    name:"Ravi",
+    age:30,
+    add:"Ahmd"
+  },
+  {
+    name:"Krupal",
+    age:27,
+    add:"Ahmd"
+  }
+]
+  clickevent(){
+    this.title = 'Jay Ho Vicky BABA ki..!!';
+  return this.title;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { UserComponent } from './user/user.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
